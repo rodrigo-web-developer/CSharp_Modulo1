9c2258f8a47e16c3ccb089504c4f8899b7a00a9e
rodrigox10 <x10rodrigo@gmail.com>
2019-01-14 17:26:04 -0200

    Aula 3 - Finalizada

---
+++ Projeto/Calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Calculadora
+    {
+        public Calculadora()
+        {
+            Console.WriteLine("Inicializando a calculadora");
+        }
+
+        public double Somar(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtrair(double x, double y = 0)
+        {
+            return x - y;
+        }
+
+        public double Somar(params double[] valores)
+        {
+            var soma = 0.0;
+            foreach (var v in valores)
+            {
+                soma += v;
+            }
+            return soma;
+        }
+
+        public void Alertar(double x)
+        {
+            Console.WriteLine("O valor de x é: "+x);
+        }
+    }
+
+}
---
+++ Projeto/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main_Aula3(string[] args)
+        {
+            var c = new Calculadora();
+
+            var x = c.Somar(10, 5);
+
+            var x1 = c.Somar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            c.Alertar(x1);
+
+            var dif1 = c.Subtrair(10, 5);
+
+            var dif2 = c.Subtrair(10);
+
+            c.Alertar(x);
+
+            var aluno = new Aluno
+            {
+                Nome = "Aluno 1",
+                DataNascimento = new DateTime(2000, 10, 01)
+            };
+
+            int idade = aluno.Idade;
+
+            Console.WriteLine($"O aluno {aluno.Nome} tem {aluno.Idade} anos de idade");
+
+            Console.ReadKey();
+        }
+        static void Main_Aula2(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var stringNormal = "C:\\Users";
+            var stringLiteral = @"C:\Users";
+            var pasta = "Users";
+            var stringInterpolate = $@"C:\{pasta}";
+
+            Console.WriteLine($"Pasta: {stringInterpolate}");
+
+            var numeros = new[] { 0, 2, 4, 5, 7 };
+
+            var lista = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite um numero");
+                lista.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine($"Voce digitou {item}");
+            }
+
+            var telefone = 11999999999;
+
+            Console.WriteLine(string.Format("O telefone e: {0:(##)#####-####}", telefone));
+
+            Console.ReadKey();
+        }
+    }
+}
